e5773b141ede9063d6eb339b8b27e6083bec501c
dastanchiik <dosyafree32@gmail.com>
2024-11-21 17:12:09 +0600

    get sum

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Men Dastanmyn");
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println(sum);
     }
 }

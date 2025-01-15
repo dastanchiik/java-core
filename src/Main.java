639a511f59d98b79ec98ed305e26e0d4501fab26
dastanchiik <dosyafree32@gmail.com>
2025-01-15 16:00:16 +0600

    java core

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,6 @@
-import java.util.Objects;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int mod1 = a%10;
-        int mod2 = (a/10)%10;
-        int mod3 = (a/100)%10;
-        int mod4 = (a/1000)%10;
-        if (mod1 == mod4 && mod2 == mod3){
-            System.out.println("YES");
-        }else {
-            System.out.println("NO");
-        }
+        System.out.println("Java core");
     }
     }
-
